486306ba3608e0f7388fdd1cec247ecd69e43385
Sabari raj <114046096+sabariraj01@users.noreply.github.com>
2023-09-02 19:24:38 +0530

    Update Traffic-System.java
    Implementation of a traffic agent.
    -Gets input from a sensor simulated file and gives output 
      accordingly.
    -This agent is associated with four roads and only one road 
      should be opened at a time and other three should be closed 
      with red signal.

--- week4/Traffic-System.java
+++ week4/Traffic-System.java
@@ -1,1 +1,40 @@
+import java.io.*;
 
+public class asgn4_1 
+{
+    public static void main(String[] ar) throws Exception 
+    {
+        Traffic_Signal o = new Traffic_Signal();
+        File f = new File("Image_Sensor.txt");
+        BufferedReader br = new BufferedReader(new FileReader(f));
+
+        int inputChar;
+        while ((inputChar = br.read()) != -1) 
+        {
+            int route = Character.getNumericValue(inputChar);
+            System.out.println("Sensor input : " + route);
+            o.change_signal(route - 1);
+        }
+        br.close();
+    }
+}
+
+class Traffic_Signal 
+{
+    char signal_light[] = { 'R', 'R', 'R', 'R' };
+
+    void change_signal(int route) throws Exception
+     {
+        signal_light[route] = 'G';
+        display();
+        Thread.sleep(3000);
+        signal_light[route] = 'R';
+    }
+
+    void display() 
+    {
+        for (int i = 0; i < 4; i++)
+            System.out.print(signal_light[i] + " ");
+        System.out.println();
+    }
+}
